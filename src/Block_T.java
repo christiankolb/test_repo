321deef20a7330bbae1bdb861ea172d051c3fd1c
christiankolb <christiankolb@gmx.net>
2015-01-10 00:35:42 +0100

    Update Block_T.java
    Konstruktor korrigiert

--- src/Block_T.java
+++ src/Block_T.java
@@ -3,19 +3,21 @@
  */
 public class Block_T extends Block {
 
-    public Block_T(){
-        block = new boolean[3][3];
-        block[0][0] = true;
-        block[0][1] = true;
-        block[0][2] = true;
-        block[1][0] = false;
-        block[1][1] = true;
-        block[1][2] = false;
-        block[2][0] = false;
-        block[2][1] = false;
-        block[2][2] = false;
-        
-        shape = T;
-    }
+	public Block_T(Shape s) {
+		super(s);
+		block = new boolean[3][3];
+		block[0][0] = true;
+		block[0][1] = true;
+		block[0][2] = true;
+		block[1][0] = false;
+		block[1][1] = true;
+		block[1][2] = false;
+		block[2][0] = false;
+		block[2][1] = false;
+		block[2][2] = false;
 
-}
+		// shape = T;
+
+	} // end constructor Block T
+	
+} // end class T
