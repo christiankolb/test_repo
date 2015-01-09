dc2b034876ae7c4b3d1198b87f49d8951715faee
christiankolb <christiankolb@gmx.net>
2015-01-10 00:44:02 +0100

    Update Block_T.java

--- src/Block_T.java
+++ src/Block_T.java
@@ -1,24 +1,18 @@
 /**
  * Created by patrick on 22.12.14.
  */
-public class Block_Z extends Block {
+public class Block_T extends Block {
 
-	public Block_Z(Shape s) 
-	{
+	public Block_T(Shape s) {
 		super(s);
-		
 		block = new boolean[3][3];
 		block[0][0] = true;
 		block[0][1] = true;
-		block[0][2] = false;
+		block[0][2] = true;
 		block[1][0] = false;
 		block[1][1] = true;
-		block[1][2] = true;
-		block[2][0] = false;
-		block[2][1] = false;
-		block[2][2] = false;
+		block[1][2] = false;
+		
+	} End constructor T
 
-		// Shape = Z;
-	}// end constrcutor Block_Z
-
-} // End class Block_Z
+} // End class Block_T
