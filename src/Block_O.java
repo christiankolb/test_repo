500a011907d991d94ad2b8dc0fa4399e4f912b78
christiankolb <christiankolb@gmx.net>
2015-01-10 00:33:44 +0100

    Update Block_O.java
    Konstruktor korrigiert

--- src/Block_O.java
+++ src/Block_O.java
@@ -3,24 +3,29 @@
  */
 public class Block_O extends Block {
 
-    public Block_O(){
-        block = new boolean[2][2];
-        block[0][0] = true;
-        block[0][1] = true;
-        block[1][0] = true;
-        block[1][1] = true;
-        
-        shape = O;
-    }
+	public Block_O(Shape s) {
+		
+		super(s);
+		
+		block = new boolean[2][2];
+		block[0][0] = true;
+		block[0][1] = true;
+		block[1][0] = true;
+		block[1][1] = true;
 
-    // keine drehung notwending
-    @Override
-    public void rotateRight() {
+		// shape = O;
+	}
 
-    }
+	// keine drehung notwending
+	
+	/* @Override
+	public void rotateRight() {
 
-    // keine drehung notwending
-    @Override
-    public void rotateLeft() {
-    }
-}
+	}
+
+	// keine drehung notwending
+	@Override
+	public void rotateLeft() {
+	} */
+	
+} // end class Block_O
