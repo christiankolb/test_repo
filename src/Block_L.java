5705109abf3e2a89753dee2820b4b4b773dbdcac
christiankolb <christiankolb@gmx.net>
2015-01-10 00:33:09 +0100

    Update Block_L.java
    Konstruktor korrigiert

--- src/Block_L.java
+++ src/Block_L.java
@@ -3,17 +3,20 @@
  */
 public class Block_L extends Block {
 
-    public Block_L(){
-        block = new boolean[3][3];
-        block[0][0] = false;
-        block[0][1] = true;
-        block[0][2] = false;
-        block[1][0] = false;
-        block[1][1] = true;
-        block[1][2] = false;
-        block[2][0] = false;
-        block[2][1] = true;
-        block[2][2] = true;
-        shape = L;
-    }
-}
+	public Block_L(Shape s) {
+		
+		super(s);
+		
+		block = new boolean[3][3];
+		block[0][0] = false;
+		block[0][1] = true;
+		block[0][2] = false;
+		block[1][0] = false;
+		block[1][1] = true;
+		block[1][2] = false;
+		block[2][0] = false;
+		block[2][1] = true;
+		block[2][2] = true;
+	
+	} // end constructor Block L
+} // end class L
