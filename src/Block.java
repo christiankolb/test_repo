0400349338da4f65cd436b9d5e0c6cbd0316a53c
christiankolb@gmx.net <m1a1abrams>
2015-01-14 08:01:22 +0100

    some stuff

--- src/Block.java
+++ src/Block.java
@@ -4,16 +4,39 @@
  * Tetris 
  * @version 1.0
  */
+
+/**
+ * 
+ * Die BLock-Klasse enthaelt alle Methoden zum verhalten eines Spielsteins.
+ * Ausgefuehrt werden diese Methoden hauptsaechlich von der Klasse BlockHandler aus, siehe {@link BlockHandler}
+ */
+
 public abstract class Block {
 
 	protected boolean[][] block;
 	
 	//////////////////////////////////
-	// Position im Feld vom Quadrat-Block des ersten K�stchens oben links
+	/** 
+	 * posrow = Position im Feld vom Quadrat-Block des ersten Kaesstchens oben links (Zeilewert)
+	 */
 	private int posrow = 0;
+	
+	/** 
+	 * poscol = Position im Feld vom Quadrat-Block des ersten Kaesstchens oben links (Spaltenwert)
+	 */
+	
 	private int poscol = 0;
-	/////////////////////////////////
+	
+	/** 
+	 * shape = Die Form des Steins
+	 */
+	
 	protected Shape shape;
+	
+	/** 
+	 * finished = Gibt an ob ein Block gesetzt wurde
+	 */
+	
 	public boolean finished;
 
 
@@ -22,41 +45,77 @@ public abstract class Block {
 		this.shape = s;
 	}
 
+	/** 
+	 * Methode die den Block zurueckgibt
+	 * @return Hat den Spielstein als Rueckgabewert
+	 */
+	
 	public boolean[][] getBlock() {
 		return this.block;
 	}
 
+	/** 
+	 * Setter-Methode zum Blockobjekt
+	 */
+	
 	public void setBlock(boolean[][] b) {
 		this.block = b;
 	}
 
+	/** 
+	 * Legt die Position im Feld vom Quadrat-Block des ersten Kaesstchens oben links fest (Zeilewert)
+	 */
+	
 	public void setPosrow(int p) {
 		this.posrow = p;
 	}
-
+	/** 
+	 * Legt die Position im Feld vom Quadrat-Block des ersten Kaesstchens oben links fest (Spaltenwert)
+	 */
+	
 	public void setPoscol(int p) {
 		this.poscol = p;
 	}
 
+	/** 
+	 * @return Gibt die Position im Feld vom Quadrat-Block des ersten K&auml;stchens oben links zurueck (Zeilenenwert)
+	 */
+	
 	public int getPosrow() {
 		return this.posrow;
 	}
+
+	/** 
+	 * @return Gibt Gibt die Position im Feld vom Quadrat-Block des ersten K&auml;stchens oben links zurueck (Spaltenwert)
+	 */
 
 	public int getPoscol() {
 		return this.poscol;
 	}
 
+	
+	/** 
+	 * Legt die Form des Spielsteins fest
+	 */
+	
+	
 	public void setShape(Shape s) {
 		this.shape = s;
 	}
 
+	/** 
+	 * @return Gibt die Form des Spielsteins zurueck
+	 */
+	
 	public Shape getShape() {
 		return this.shape;
 	}
 	
 
-	/*
-	* bewegt das Array um 1 Position nach unten
+	/**
+	* bewegt das Array um 1 Position nach unten im Spielfeld
+	* @param field = Das Spielfeld wird hierbei uebergeben
+	* @return &Uuml;berpr&uuml;ft und gibt zurueck ob die Bewegung nach unten noch moeglich ist
 	*/
 	////// Patricks Version
 	public boolean moveDown(Field field){
@@ -90,10 +149,10 @@ public abstract class Block {
 	}
 
 
-
-
-	/*
-	* BUGGY bewegt das Array um 1 Position nach rechts
+	/**
+	* Bewegt den Block (Spielstein) um 1 Position nach rechts
+	* @param field = Das Spielfeld wird hierbei uebergeben
+	* @return &Uuml;berpr&uuml;ft und gibt zurueck ob die Bewegung nach rechts noch moeglich ist
 	*/
 	//////Patricks Version
 	public boolean moveRight(Field field){
@@ -124,8 +183,10 @@ public abstract class Block {
 		}
 	}
 
-	/*
-	* bewegt das Array um 1 Position nach links
+	/**
+	* bewegt den Block um 1 Position nach links
+	* @param field = Das Spielfeld wird hierbei uebergeben
+	* @return &Uuml;berpr&uuml;ft und gibt zurueck ob die Bewegung nach links noch moeglich ist
 	*/
 	//////Patricks Version
 	public boolean moveLeft(Field field){
@@ -156,8 +217,10 @@ public abstract class Block {
 	}
 
 	
-	/*
-	* rotiert array nach rechts
+	/**
+	* rotiert den Block nach rechts im Spielfeld
+	* @param field = Spielfeld sehe {@link Field}
+	* @return Gibt zurueck ob die Drehung auch durchgefuehrt werden konnte
 	*/
 	public boolean rotateRight(Field field){
 		boolean[][] rotated = new boolean[this.block[0].length][this.block.length];
@@ -169,8 +232,10 @@ public abstract class Block {
 		return this.isRotationPossible(rotated, field);
 	}
 
-	/*
-	* rotiert array nach links
+	/**
+	* rotiert den Block im Field nach links
+	* @param field = Spielfeld sehe {@link Field}
+	* @return Gibt zurueck ob die Drehung auch durchgefuehrt werden konnte
 	*/
 	public boolean rotateLeft(Field field){
 		boolean[][] rotated = new boolean[this.block[0].length][this.block.length];
@@ -182,9 +247,15 @@ public abstract class Block {
 		return this.isRotationPossible(rotated, field);
 	}
 
-	/*
-	*	überprüfen ob drehung möglich
-	*/
+	
+	
+	
+	/**
+	 * Überprüfen ob drehung möglich
+	 * @param rotatet: Ist der Spielstein in rotierter Form, field ist das Spielfeld
+	 * @return Gibt einen Booleanwert zurueck, der anzeigt ob er der rotierte Stein auch "Platz" hat im Spielfeld, also ob die Rotation ueberhaupt moeglich ist
+	 */
+	
 	public boolean isRotationPossible(boolean[][] rotated,Field field) {
 		boolean possible = true;
 		boolean chpos = false;
