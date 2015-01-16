197b8146ab93331a5b297e302c71fe64aa9b2ed3
Dragomirka <Dragomirka@192.168.1.12>
2015-01-16 20:16:02 +0100

    Update

--- src/Printing.java
+++ src/Printing.java
@@ -9,6 +9,7 @@ public class Printing {
 	 * dennoch geben wir das Spielfeld nicht als Boolean-Werte aus, sondern als Character
 	 * Dabei wird je nachdem, welchen Wert der Stein aus der {@link Shape}-Enum hat, werden
 	 * andere, zum Spielstein passende, Zeichen ausgegeben.
+	 * @param field: Feld, welches gedruckt wird
 	 */
 	
 	public void printField(Field field) {
@@ -48,15 +49,33 @@ public class Printing {
 
 	} // end printField
 	
+	
+	/**
+	 * Druckt Informationen über den nächsten Block
+	 * @param current: der nächste Block
+	 */
 	public void printSpawnNextBlock(Block current ){
 	      System.out.println("\nNeuer Block: " + current.getShape() + ", an der Position  " + current.getPoscol() + "/" + current.getPosrow());
 	      
 	}
 	
+	
+	/**
+	 * Druckt den Versuch der Bewegung
+	 * @param moveDis: Anzahl der Kästchen, die bewegt wurden
+	 * @param direction: Richtung der Bewegung
+	 */
 	public void printTryMove(int moveDis, String direction){
 		 System.out.println("Versuche Bewegung um " + moveDis + " nach "+direction);
 	}
 	
+	
+	/**
+	 * Druckt ob die Bewegung möglich war
+	 * @param moveDis: Anzahl der Kästchen, die bewegt wurden
+	 * @param direction: Richtung der Bewegung
+	 * @param possible: ob es möglich war
+	 * 	 */
 	public void printMovePossible(int moveDis,String direction, boolean possible){
 		
 		if (!possible){
@@ -66,9 +85,22 @@ public class Printing {
 		}
 		
 	}
+	
+	/**
+	 * Druckt den Versuch der Rotation
+	 * @param degree: Grad der Rotation
+	 * @param direction: Richtung der Rotation
+	 *  */
 		public void printTryRotation(int degree, String direction){
 			System.out.println("Versuche Rotation um " + degree + "Â° "+direction+" UZS");
 		}
+		
+		
+		/**
+		 * Druckt, ob Rotation möglich ist
+		 * @param direction: Richtung der Rotation
+		 * @param possible: war die Rotation möglich
+		 */
            
 		public void printRotationPossible(String direction, boolean possible){
 			if(!possible){
@@ -77,10 +109,18 @@ public class Printing {
 				System.out.println("-- Rotation nach "+direction+" mÃ¶glich");
 			}
 		}
+		
+		/**
+		 * Druckt, wenn Reihen aufgelöst worden sind
+		 * @param anzahl: Anzahl der Reihen
+	 */
 	public void printDeleteFullRows(int anzahl){
 		System.out.println(anzahl+" vollständige Reihen wurden aufgelöst.");
 	}
 	
+	/**
+	 * Druckt, wenn ein Stein nach der Rotation ins Spielfeld gezogen wurde
+	 */
 	public void printRotationMove(){
 		System.out.println("Stein wurde fÃ¼r die folgende Rotation ins Spielfeld gezogen");
 	}
